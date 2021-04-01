811e8d42b03b78ab2e0f605105c59814ef69b170
Bernardo Sertório <besertorio@gmail.com>
2021-04-01 13:32:16 -0300

    GlobalStyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import GlobalStyle from './styles/global';
+
 export function App() {
   return (
     <div className="App">
-     <h1>Hello word</h1>
+     <GlobalStyle />
     </div>
   );
 }
---
+++ src/styles/global.ts
@@ -0,0 +1,39 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+  }
+
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%;
+    }
+
+    @media (max-width: 720px) {
+      font-size: 87.5%;
+    }
+  }
+
+  body {
+    background: #fff;
+    color: #FFF;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font-family: 'Poppins', sans-serif;
+    font-size: 16px;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 500;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
