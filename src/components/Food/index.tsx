1a63ea5dce57317764a71721275a8528d011a7db
Bernardo Sertório <besertorio@gmail.com>
2021-04-01 19:07:45 -0300

    setting axios and mirageJs

--- src/components/Food/index.tsx
+++ src/components/Food/index.tsx
@@ -1,48 +1,54 @@
+import { useEffect } from 'react';
 import { FiEdit3, FiTrash } from 'react-icons/fi';
+import { api } from '../../services/api';
 import { Container } from './styles';
 
 export function Food() {
-    return (
-      <Container>
-        <header>
-          <img src="https://storage.googleapis.com/golden-wind/bootcamp-gostack/desafio-food/food1.png" alt=""/>
-        </header>
-        <section className="body">
-          <h2>Macarrão</h2>
-          <p>com carne</p>
-          <p className="price">
-            R$ <b>R$200,00</b>
-          </p>
-        </section>
-        <section className="footer">
-          <div className="icon-container">
-            <button
-              type="button"
-              className="icon"
-            >
-              <FiEdit3 size={20} />
-            </button>
+  useEffect(() => {
+    api.get('/transactions').then(response => console.log(response.data))
+  }, []);
 
-            <button
-              type="button"
-              className="icon"
-            >
-              <FiTrash size={20} />
-            </button>
-          </div>
+  return (
+    <Container>
+      <header>
+        <img src="https://storage.googleapis.com/golden-wind/bootcamp-gostack/desafio-food/food1.png" alt="" />
+      </header>
+      <section className="body">
+        <h2>Macarrão</h2>
+        <p>com carne</p>
+        <p className="price">
+          R$ <b>200,00</b>
+        </p>
+      </section>
+      <section className="footer">
+        <div className="icon-container">
+          <button
+            type="button"
+            className="icon"
+          >
+            <FiEdit3 size={20} />
+          </button>
 
-          <div className="availability-container">
-            <p>{'Disponível' || 'Indisponível'}</p>
+          <button
+            type="button"
+            className="icon"
+          >
+            <FiTrash size={20} />
+          </button>
+        </div>
 
-            <label className="switch">
-              <input
-                type="checkbox"
-              />
-              <span className="slider" />
-            </label>
-          </div>
-        </section>
-      </Container>
-    );
-  };
+        <div className="availability-container">
+          <p>{'Disponível' || 'Indisponível'}</p>
+
+          <label className="switch">
+            <input
+              type="checkbox"
+            />
+            <span className="slider" />
+          </label>
+        </div>
+      </section>
+    </Container>
+  );
+};
 
