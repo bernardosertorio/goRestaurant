2616de36247a962d3de9d1f0053390f198c342c4
Bernardo Sertório <besertorio@gmail.com>
2021-04-05 13:46:34 -0300

    creating react modal and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,15 @@
-import GlobalStyle from './styles/global';
-import Modal from 'react-modal';
-import { Header } from './components/Header';
-import { Dashboard } from './pages/Dashboard';
-import { useState } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-Modal.setAppElement('#root');
+import Routes from './routes';
+
+import GlobalStyle from './styles/global';
 
 export function App() {
-  const [isNewCreateFoodModalOpen, setIsNewCreateFoodModalOpen] = useState(false);
-
-  function handleOpenNewCreateFoodModal() {
-    setIsNewCreateFoodModalOpen(true)
-  }
-
-  function handleCloseNewCreateFoodModal() {
-    setIsNewCreateFoodModalOpen(false)
-  }
-
   return (
     <>
-      <Header onOpenNewCreateFoodModal={handleOpenNewCreateFoodModal}/>
-      <Dashboard />
-      <Modal
-        isOpen={isNewCreateFoodModalOpen}
-        onRequestClose={handleCloseNewCreateFoodModal}
-      >
-        <h2>Criar novo prato</h2>
-      </Modal>
-      
+      <Router>
+        <Routes />
+      </Router>
       <GlobalStyle />
     </>
   );
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,21 @@
 import { Food } from '../../components/Food';
+import { Header } from '../../components/Header';
 import { FoodsContainer } from './styles';
+import { useState } from 'react';
 
 export function Dashboard() {
+  const [isNewCreateFoodModalOpen, setIsNewCreateFoodModalOpen] = useState(false);
+
+  function handleOpenNewCreateFoodModal() {
+    setIsNewCreateFoodModalOpen(true)
+  }
+
+  function handleCloseNewCreateFoodModal() {
+    setIsNewCreateFoodModalOpen(false)
+  }
     return (
       <FoodsContainer>
+        <Header onOpenNewCreateFoodModal={handleOpenNewCreateFoodModal}/>
         <Food />
       </FoodsContainer>
     );
